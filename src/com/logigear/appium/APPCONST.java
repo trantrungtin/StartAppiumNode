94bea61cb8c1807b4fcee3a6da42be7ea8fa2497
Trung Tin Tran <tinonsoftware@gmail.com>
2017-03-16 22:03:34 +0700

    add missing file

---
+++ src/com/logigear/appium/APPCONST.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.logigear.appium;
+
+/**
+ *
+ * @author Tin
+ */
+public class APPCONST {
+    public final static String ANDROID = "Android";
+    public final static String IOS = "iOS";
+}
